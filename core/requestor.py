cb874ca74aefc8f6c7e5af16b00daea19bf642d0
Ansan Binoy <58063896+ansanbinoy@users.noreply.github.com>
2020-11-11 07:46:23 +0530

    Update requestor.py

--- core/requestor.py
+++ core/requestor.py
@@ -15,13 +15,13 @@ class Req(Thread):
                 list = []
                 try:
                         x = requests.get(self.url,timeout=1001)
-                        resp = "[+] "+self.url+"  \t  "+str(x.status_code)
+                        resp = self.url+"  \t  "+str(x.status_code)
 
                         print (resp)
                         list.append(resp)
                 except:
-                        print (error+red+self.url+"\tFailed to connect!."+reset)
-#                       pass
+#                        print (error+red+self.url+"\tFailed to connect!."+reset)
+                        pass
 def launch(urls,th,file=None):
         for i in range(len(urls)):
                 if 1 < 100:
